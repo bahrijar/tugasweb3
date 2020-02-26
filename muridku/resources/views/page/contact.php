4c1a6fd5eb5935189770e6dbb2ccbd07c4ac5008
bahri <muhammadbahrijar1@gmail.com>
2020-02-26 23:50:00 +0700

    ad
    sa

---
+++ muridku/resources/views/page/contact.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+
+		<!-- Description -->
+		<meta name="description" content="Metronal is a clean and elegant Personal Portfolio Page. Metronal comes with a very attractive, simple and beautiful design with the effectiveness of colorful simplicity.">
+
+		<!-- Keywords -->
+		<meta name="keywords" content="Metronal, Personal Page, Portfolio, CV, Resume, One Page, Web Template">
+
+		<!-- Title -->
+		<title>Muhammad Bahrijar</title>
+
+		<!-- Google Fonts -->
+		<link href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,800%7COpen+Sans:400,400i,700" rel="stylesheet">
+
+		<!-- CSS Files -->
+		<link href="/css/bootstrap.min.css" rel="stylesheet">
+		<link href="/css/fontawesome/css/all.min.css" rel="stylesheet">
+		<link href="/css/magnific-popup.css" rel="stylesheet">
+		<link href="/css/skins/skin3.css" rel="stylesheet">
+		<link href="/css/custom.css" rel="stylesheet">
+</head>
+<body>
+
+<!-- Pre Load Starts -->
+<div id="pre-load">
+	    	<div class="load-circle"></div>
+	    </div>
+	    <!-- Pre Load Ends -->
+  <!-- Main Content (About) Starts -->
+  <div id="wrapper">
+			<div class="container-fluid">
+				<div class="row no-gutters v-full">
+					<!-- Left Wrapper Starts -->
+					<div id="left-wrapper" class="col-12 col-lg-8">
+						<!-- Main Content (Contact) Starts -->
+						<div id="contact" class="main-content">
+							<!-- Content Hanging On Contact Section Starts -->
+							<div class="hanging">
+								<div class="logo-hanging">
+									<i class="fas fa-envelope"></i>
+								</div>
+								<div class="text-hanging">
+									<div class="word">
+										<h6>CONTACT <span>ME</span></h6>
+									</div>
+								</div>
+							</div>
+							<!-- Content Hanging On Contact Section Ends -->
+							<!-- Inner Content Starts -->
+							<div class="inner-content">
+								<!-- Head Content Starts -->
+								<div class="head-content">
+									<h3>Contact <span>Me</span></h3>
+								</div>
+								<!-- Head Content Ends -->
+								<!-- Content Starts -->
+								<div class="content">
+									<!-- Contact Info Starts -->
+									<div id="contact-info">
+										<!-- Contact Info Heading Starts -->
+										<h5>Contact Info</h5>
+										<!-- Contact Info Heading Ends -->
+										<div class="row no-gutters">
+											<!-- List Contact Info Starts -->
+											<div class="list-contact-info col-12">
+												<ul class="row no-gutters">
+													<!-- Single Contact Info Starts -->
+													<li class="col-sm-6 col-12">
+														<span class="contact-info-name">Address</span>
+														<span class="icon-of-value">
+															<i class="fas fa-map-marker-alt"></i>
+														</span>
+														<span class="value">Cakung Timur, ID</span>
+													</li>
+													<!-- Single Contact Info Ends -->
+													<!-- Single Contact Info Starts -->
+													<li class="col-sm-6 col-12">
+														<span class="contact-info-name">Phone</span>
+														<span class="icon-of-value">
+															<i class="fas fa-mobile-alt"></i>
+														</span>
+														<span class="value">+62 813 8396 8265</span>
+													</li>
+													<!-- Single Contact Info Ends -->
+													<!-- Single Contact Info Starts -->
+													<li class="col-sm-6 col-12">
+														<span class="contact-info-name">Email</span>
+														<span class="icon-of-value">
+															<i class="fas fa-envelope"></i>
+														</span>
+														<span class="value">muhammadbahrijar4@gmail.com</span>
+													</li>
+													<!-- Single Contact Info Ends -->
+													<!-- Single Contact Info Starts -->
+													<li class="col-sm-6 col-12">
+														<span class="contact-info-name">Website</span>
+														<span class="icon-of-value">
+															<i class="fas fa-globe"></i>
+														</span>
+														<span class="value">www.muhammadbahrijar.website</span>
+													</li>
+													<!-- Single Contact Info Ends -->
+													<!-- Single Contact Info Starts -->
+													<li class="col-12">
+														<span class="contact-info-name">Social Media</span>
+														<ul class="social-media">
+															<!-- Single Social Media Starts -->
+															<li>
+																<a href="#">
+																	<span class="front">
+																		<i class="fab fa-facebook-f"></i>
+																	</span>
+																	<span class="back">
+																		<i class="fab fa-facebook-f"></i>
+																	</span>
+																</a>
+															</li>
+															<!-- Single Social Media Ends -->
+															<!-- Single Social Media Starts -->
+															<li>
+																<a href="#">
+																	<span class="front">
+																		<i class="fab fa-twitter"></i>
+																	</span>
+																	<span class="back">
+																		<i class="fab fa-twitter"></i>
+																	</span>
+																</a>
+															</li>
+															<!-- Single Social Media Ends -->
+															<!-- Single Social Media Starts -->
+															<li>
+																<a href="#">
+																	<span class="front">
+																		<i class="fab fa-instagram"></i>
+																	</span>
+																	<span class="back">
+																		<i class="fab fa-instagram"></i>
+																	</span>
+																</a>
+															</li>
+															<!-- Single Social Media Ends -->
+															<!-- Single Social Media Starts -->
+															<li>
+																<a href="#">
+																	<span class="front">
+																		<i class="fab fa-linkedin-in"></i>
+																	</span>
+																	<span class="back">
+																		<i class="fab fa-linkedin-in"></i>
+																	</span>
+																</a>
+															</li>
+															<!-- Single Social Media Ends -->
+                                                        </ul>
+                                                        
+													</li>
+                                                    <!-- Single Contact Info Ends -->
+                                                    <br/>
+                                                    <!-- Button Wrapper Starts -->
+											<div class="button-wrapper col-12">
+												<!-- Single Button Starts -->
+												<div class="single-button">
+													<a href="/">
+														<span class="front">
+															<i class="fas fa-arrow-left"></i><span class="value"><span> Homepage </span></span>
+														</span>
+														
+													</a>
+												</div>
+												<!-- Single Button Ends -->
+									
+											</div>
+											<!-- Button Wrapper Ends -->
+                                                </ul>
+                    
+											</div>
+											<!-- List Contact Info Ends -->
+										</div>
+									</div>
+									<!-- Contact Info Ends -->
+								
+								</div>
+								<!-- Content Ends -->
+							</div>
+							<!-- Inner Content Ends -->
+						</div>
+						<!-- Main Content (Contact) Ends -->
+
+                    </div>
+					<!-- Left Wrapper Ends -->
+					<!-- Right Wrapper Starts -->
+					<div id="right-wrapper" class="col-lg-4">
+						<!-- Large Profile Picture Starts -->
+						<div class="lg-profile-picture"></div> 
+						<!-- Large Profile Picture Ends -->
+					</div>	
+					<!-- Right Wrapper Ends -->
+				</div>
+			</div>
+		</div>
+		<!-- Wrapper Ends -->
+                        
+                        <script src="js/jquery-3.3.1.min.js"></script>
+		<script src="/js/bootstrap.min.js"></script>
+		<script src="/js/imagesloaded.pkgd.min.js"></script>
+		<script src="/js/isotope.pkgd.min.js"></script>
+		<script src="/js/magnific-popup.min.js"></script>
+		<script src="/js/typeit.min.js"></script>
+		<script src="/js/custom.js"></script>
+</body>
+</html>
---
+++ muridku/resources/views/page/profile.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+
+		<!-- Description -->
+		<meta name="description" content="Metronal is a clean and elegant Personal Portfolio Page. Metronal comes with a very attractive, simple and beautiful design with the effectiveness of colorful simplicity.">
+
+		<!-- Keywords -->
+		<meta name="keywords" content="Metronal, Personal Page, Portfolio, CV, Resume, One Page, Web Template">
+
+		<!-- Title -->
+		<title>Muhammad Bahrijar</title>
+
+		<!-- Google Fonts -->
+		<link href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,800%7COpen+Sans:400,400i,700" rel="stylesheet">
+
+		<!-- CSS Files -->
+		<link href="/css/bootstrap.min.css" rel="stylesheet">
+		<link href="/css/fontawesome/css/all.min.css" rel="stylesheet">
+		<link href="/css/magnific-popup.css" rel="stylesheet">
+		<link href="/css/skins/skin3.css" rel="stylesheet">
+		<link href="/css/custom.css" rel="stylesheet">
+</head>
+<body>
+
+<!-- Pre Load Starts -->
+<div id="pre-load">
+	    	<div class="load-circle"></div>
+	    </div>
+	    <!-- Pre Load Ends -->
+  <!-- Main Content (About) Starts -->
+  <div id="wrapper">
+			<div class="container-fluid">
+				<div class="row no-gutters v-full">
+					<!-- Left Wrapper Starts -->
+					<div id="left-wrapper" class="col-12 col-lg-8">
+						<!-- Main Content (Home) Starts -->
+  <div id="about" class="main-content">
+							<!-- Content Hanging On About Section Starts -->
+							<div class="hanging">
+								<div class="logo-hanging">
+									<i class="fas fa-id-card"></i>
+								</div>
+								<div class="text-hanging">
+									<div class="word">
+										<h6>MY <span>PROFILE</span></h6>
+									</div>
+								</div>
+							</div>
+							<!-- Content Hanging On About Section Ends -->
+							<!-- Inner Content Starts -->
+							<div class="inner-content">
+								<!-- Head Content Starts -->
+								<div class="head-content">
+									<h3>My <span>Profile</span></h3>
+								</div>
+								<!-- Head Content Ends -->
+								<!-- Content Starts -->
+								<div class="content">
+									<!-- Personal Info Starts -->
+									<div id="personal-info">
+										<!-- Personal Info Heading Starts -->
+										<h5>Personal Info</h5>
+										<!-- Personal Info Heading Ends -->
+										<div class="row no-gutters">
+											<!-- Profile Picture Starts -->
+											<div class="profile-picture col-md-2 col-sm-3 col-12"></div>
+											<!-- Profile Picture Ends -->
+											<!-- Summary Starts -->
+											<div class="summary col-md-10 col-sm-9 col-12">
+												<!-- Single Paragraph Starts -->
+												<p>I am a UI/UX Designer &amp; Web & Android Developer from Jakarta, ID. I am very passionate and dedicated to my work. I really enjoy working in a team or individual.</p>
+												<!-- Single Paragraph Ends -->
+											</div>
+											<!-- Summary Ends -->
+											<!-- Single Profile Starts -->
+											<div class="profile col-12 col-sm-6">
+												<ul>
+													<!-- Single Content On Profile Starts -->
+													<li>
+														<span class="label">
+															<i class="fas fa-angle-double-right"></i><span>First Name</span>
+														</span>
+														<span class="dash">-</span>
+														<span class="value">Muhammad</span>
+													</li>
+													<!-- Single Content On Profile Ends -->
+													<!-- Single Content On Profile Starts -->
+													<li>
+														<span class="label">
+															<i class="fas fa-angle-double-right"></i><span>Last Name</span>
+														</span>
+														<span class="dash">-</span>
+														<span class="value">Bahrijar</span>
+													</li>
+													<!-- Single Content On Profile Ends -->
+													<!-- Single Content On Profile Starts -->
+													<li>
+														<span class="label">
+															<i class="fas fa-angle-double-right"></i><span>Date of Birth</span>
+														</span>
+														<span class="dash">-</span>
+														<span class="value">19 October 1999</span>
+													</li>
+													<!-- Single Content On Profile Ends -->
+													<!-- Single Content On Profile Starts -->
+													<li>
+														<span class="label">
+															<i class="fas fa-angle-double-right"></i><span>Nationality</span>
+														</span>
+														<span class="dash">-</span>
+														<span class="value">Indonesia</span>
+													</li>
+													<!-- Single Content On Profile Ends -->
+												</ul>
+											</div>
+											<!-- Single Profile Ends -->
+											<!-- Single Profile Starts -->
+											<div class="profile col-12 col-sm-6">
+												<ul>
+													<!-- Single Content On Profile Starts -->
+													<li>
+														<span class="label">
+															<i class="fas fa-angle-double-right"></i><span>Phone</span>
+														</span>
+														<span class="dash">-</span>
+														<span class="value">+62 813 8396 8265</span>
+													</li>
+													<!-- Single Content On Profile Ends -->
+													<!-- Single Content On Profile Starts -->
+													<li>
+														<span class="label">
+															<i class="fas fa-angle-double-right"></i><span>Email</span>
+														</span>
+														<span class="dash">-</span>
+														<span class="value">muhammadbahrijar4@gmail.com</span>
+													</li>
+													<!-- Single Content On Profile Ends -->
+													<!-- Single Content On Profile Starts -->
+													<li>
+														<span class="label">
+															<i class="fas fa-angle-double-right"></i><span>Address</span>
+														</span>
+														<span class="dash">-</span>
+														<span class="value">Cakung Jakarta timur, ID</span>
+													</li>
+													<!-- Single Content On Profile Ends -->
+													<!-- Single Content On Profile Starts -->
+													<li>
+														<span class="label">
+															<i class="fas fa-angle-double-right"></i><span>Languages</span>
+														</span>
+														<span class="dash">-</span>
+														<span class="value">Bahasa Indonesia, English</span>
+													</li>
+													<!-- Single Content On Profile Ends -->
+												</ul>
+											</div>
+											<!-- Single Profile Ends -->
+											<!-- Social Media Starts -->
+											<div class="social-media col-12">
+												<ul>
+													<!-- Single Social Media Starts -->
+													<li>
+														<a href="https://facebook.com/ijar.kartika" target="_blank">
+															<span class="front">
+																<i class="fab fa-facebook-f"></i>
+															</span>
+															<span class="back">
+																<i class="fab fa-facebook-f"></i>
+															</span>
+														</a>
+													</li>
+													<!-- Single Social Media Ends -->
+													<!-- Single Social Media Starts -->
+													<li>
+														<a href="#">
+															<span class="front">
+																<i class="fab fa-twitter"></i>
+															</span>
+															<span class="back">
+																<i class="fab fa-twitter"></i>
+															</span>
+														</a>
+													</li>
+													<!-- Single Social Media Ends -->
+													<!-- Single Social Media Starts -->
+													<li>
+														<a href="https://instagram.com/m.bhrjr" target="_blank">
+															<span class="front">
+																<i class="fab fa-instagram"></i>
+															</span>
+															<span class="back">
+																<i class="fab fa-instagram"></i>
+															</span>
+														</a>
+													</li>
+													<!-- Single Social Media Ends -->
+													<!-- Single Social Media Starts -->
+													<li>
+														<a href="#">
+															<span class="front">
+																<i class="fab fa-linkedin-in"></i>
+															</span>
+															<span class="back">
+																<i class="fab fa-linkedin-in"></i>
+															</span>
+														</a>
+													</li>
+													<!-- Single Social Media Ends -->
+												</ul>
+											</div>
+											<!-- Social Media Ends -->
+											<!-- Button Wrapper Starts -->
+											<div class="button-wrapper col-12">
+												<!-- Single Button Starts -->
+												<div class="single-button">
+													<a href="/">
+														<span class="front">
+															<i class="fas fa-arrow-left"></i><span class="value"><span>Homepage </span></span>
+														</span>
+														<span class="back">
+															<i class="fas fa-arrow-left"></i><span class="value"><span>Homepage</span></span>
+														</span>
+													</a>
+												</div>
+												<!-- Single Button Ends -->
+									
+											</div>
+											<!-- Button Wrapper Ends -->
+										</div>
+									</div>
+									<!-- Personal Info Ends -->
+								</div>
+								<!-- Content Ends -->
+							</div>
+							<!-- Inner Content Ends -->
+						</div>
+                        <!-- Main Content (About) Ends -->
+
+                    </div>
+					<!-- Left Wrapper Ends -->
+					<!-- Right Wrapper Starts -->
+					<div id="right-wrapper" class="col-lg-4">
+						<!-- Large Profile Picture Starts -->
+						<div class="lg-profile-picture"></div> 
+						<!-- Large Profile Picture Ends -->
+					</div>	
+					<!-- Right Wrapper Ends -->
+				</div>
+			</div>
+		</div>
+		<!-- Wrapper Ends -->
+                        
+                        <script src="js/jquery-3.3.1.min.js"></script>
+		<script src="/js/bootstrap.min.js"></script>
+		<script src="/js/imagesloaded.pkgd.min.js"></script>
+		<script src="/js/isotope.pkgd.min.js"></script>
+		<script src="/js/magnific-popup.min.js"></script>
+		<script src="/js/typeit.min.js"></script>
+		<script src="/js/custom.js"></script>
+</body>
+</html>
